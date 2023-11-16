0628ffe152f2bd2b775013b9083e0f5bddf372ee
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-16 19:22:39 +0100

    implemented pop opcode

--- monty.h
+++ monty.h
@@ -89,7 +89,7 @@ void _exe(void);
 int _isdigit(int c);
 void free_STACK(stack_t *head);
 void _pint(stack_t **stack, unsigned int line_number);
-
+void _pop(stack_t **stack, unsigned int line_number);
 
 
 #endif /* End of include guard */
