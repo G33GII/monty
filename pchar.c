a7f64e340e534c4505e34d26f5409bad5d31ae7f
G33GII <gazasolomon@gmail.com>
2023-11-17 04:30:42 +0100

    battied all

---
+++ pchar.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * _pchar - Function to print the top element of the stack
+ * @stack: stack pointer
+ * @line_number: line number
+ * Return: void
+ */
+void _pchar(stack_t **stack, unsigned int line_number)
+{
+	int a = (*stack)->n;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((a < 0 || a > 127))
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", a);
+}
