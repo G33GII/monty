5b58eea3056b4d1d576f47973dbb650198fd170d
Shekwogaza <gazasolomon@gmail.com>
2023-11-19 12:18:36 -0800

    bettied ismonty file

--- _rotr.c
+++ _rotr.c
@@ -8,24 +8,24 @@
  */
 void _rotr(stack_t **stack, unsigned int line_number)
 {
-    stack_t *H = *stack;
-    stack_t *T = Em.STACKT;
+	stack_t *H = *stack;
+	stack_t *T = Em.STACKT;
 	int lx = 0;
 
-    (void) line_number;
-    if (H == NULL || H->prev == NULL) /* If list empty or only one NODE */
-        return;
+	(void) line_number;
+	if (H == NULL || H->prev == NULL) /* If list empty or only one NODE */
+		return;
 
-    /* Checking if theres a second string in the rotr command "rotr GERRY" */
-    if (Em.VALUE)
-        return;
-    /* Return if more than 1 tokenizable strings exist */
+	/* Checking if theres a second string in the rotr command "rotr GERRY" */
+	if (Em.VALUE)
+		return;
+	/* Return if more than 1 tokenizable strings exist */
 
-    lx = T->n;
-    while (T->next != NULL)
-    {
-        T->n = T->next->n;
-        T = T->next;
-    }
-    T->n = lx;
+	lx = T->n;
+	while (T->next != NULL)
+	{
+		T->n = T->next->n;
+		T = T->next;
+	}
+	T->n = lx;
 }
