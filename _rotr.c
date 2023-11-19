dd5a16776f06029bd8420182f40743388577846f
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-19 15:19:28 +0100

    fixed rotr

--- _rotr.c
+++ _rotr.c
@@ -8,27 +8,24 @@
  */
 void _rotr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *H = *stack;
-	stack_t *T = Em.STACKT;
-	int lx = _LENGTHLIST() / 2, x = 0, z;
+    stack_t *H = *stack;
+    stack_t *T = Em.STACKT;
+	int lx = 0;
 
-	(void)line_number;
-	if (H == NULL || H->prev == NULL) /* If list empty or only one NODE */
-		return;
+    (void) line_number;
+    if (H == NULL || H->prev == NULL) /* If list empty or only one NODE */
+        return;
 
-	/* Checking if theres a second string in the rotr command "rotr GERRY" */
-	if (Em.VALUE)
-		return;
-	/* Return if more than 1 tokenizable strings exist */
+    /* Checking if theres a second string in the rotr command "rotr GERRY" */
+    if (Em.VALUE)
+        return;
+    /* Return if more than 1 tokenizable strings exist */
 
-	while (x < lx)
-	{
-		z = H->n;
-		H->n = T->n;
-		T->n = z;
-
-		H = H->prev;
-		T = T->next;
-		x++;
-	}
+    lx = T->n;
+    while (T->next != NULL)
+    {
+        T->n = T->next->n;
+        T = T->next;
+    }
+    T->n = lx;
 }
