b2d7f98b15aa3ed3d8fad13f0d972c113753c851
_ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-19 20:09:43 +0100

    minor main update

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-/* Em   =   PUQU FP   AGV   STR   FUNC  VALUE STACKH STACKT LN*/
+/* Em   =   PUQU FP   AGV   STR   FUNC  VALUE STACKH STACKT LN */
 _MONTY Em = {0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0};
 
 /**
@@ -11,17 +11,14 @@ _MONTY Em = {0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0};
  */
 int main(int argc, char **argv)
 {
-	FILE *_fp; /* fp - file pointer */
+	FILE *_fp;	/* fp - file pointer */
 
-	/* _FILEPRESENT(argv, argc); */
 	_ismonty_file(argv, argc); /* check whether the file name is a valid file */
 	_fp = _open_file(argv[1]); /* function to fopen FILE name */
-
 	Em.FP = _fp;
 	Em.AGV = argv[1];
 
 	_cpFile(_fp);
-
 	_close_file(_fp, argv[1]);
 	free_STACK(Em.STACKH);
 	return (0);
